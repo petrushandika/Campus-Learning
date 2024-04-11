b1dfb16b1136b50ebfab4165e3e992e028697d64
petrushandika <petrushandikasinaga@gmail.com>
2024-04-11 08:54:23 +0700

    Python

--- Python/01 Numeric Method/app.py
+++ Python/01 Numeric Method/app.py
@@ -1,16 +1,17 @@
+# Find value f(1) for f(x) = e**x+3 for x0 = 0 to 5 iteration
 import math
 
-def calculateFactorial (x):
+def calculateFactorial(x):
     if x < 2:
         return 1
     else:
-        return x * calculateFactorial (x-1)
+        return x * calculateFactorial(x - 1)
     
-def iteration (n, x):
+def iteration(n, x):
     it = math.exp(3)
     
-    for i in range (1, n):
-        it = it + (math.exp(3) * x ** i) / calculateFactorial (i)
+    for i in range(1, n):
+        it = it + (math.exp(3) * x ** i) / calculateFactorial(i)
         return it
     
-print("Iteration 5 is ", iteration (5,1))
+print("Iteration 5 is ", iteration(5, 1))
